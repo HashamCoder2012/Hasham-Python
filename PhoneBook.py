917b603aa9a4ceac79af2750b3dfb5de652102d7
HashamCoder2012 <mhashamk2012@gmail.com>
2024-12-21 12:55:38 +0500

    l

--- PhoneBook.py
+++ PhoneBook.py
@@ -15,7 +15,28 @@ def initial_phonebook():
         phone_book.append(temp)
 
     return phone_book
+def add_contact(pb):
+    dip=[]
+    for i in range(len(pb[2])):
+        if i ==0:
+            dip.append(str(input("Enter name: ")))
+        if i == 1:
+            dip.append(int(input("Enter number: ")))
+    pb.append(dip)
+    return pb
 print("Welcome to PhoneBook app")
 pb=initial_phonebook()
-print(pb)                        
+print(pb)
+add=input("Do you want to add a new contact?(y/n)")
+if add=="y":
+    pb=add_contact(pb)
+    print(pb)
+
+def remove_existing(pb):
+    query=str(input("Please enter the name of the contact you wish to remove: "))
+    temp=0
+    for i in range(len(pb)):
+        if query == pb[i][0]:
+            temp +=1
+            print(pb.pop(i))
                          
