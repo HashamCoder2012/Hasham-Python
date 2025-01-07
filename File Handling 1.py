16704bbbc63f3f18e353ad11eb9c5358b2171680
HashamCoder2012 <mhashamk2012@gmail.com>
2025-01-07 21:19:19 +0500

    k

--- File Handling 1.py
+++ File Handling 1.py
@@ -2,7 +2,7 @@ file=open('C:/Users/Khurram shahzad.NAJIB-SMA/Desktop/Hasham file.txt','r')
 print(file.read())
 file.close()
 
-file=open('C:/Users/Khurram shahzad.NAJIB-SMA/Desktop/Hasham file.txt','w')
+file=open('C:/Users/Khurram shahzad.NAJIB-SMA/Desktop/Hasham file.txt','r')
 print("\n Read in parts \n")
 print(file.read(8))
 print(file.readline())
---
+++ fh2.py
@@ -0,0 +1,15 @@
+import sys
+def initial_phonebook():
+    rows, cols = int(input("Please enter initial number of contacts: ")),2
+    phone_book =[]
+    print(phone_book)
+    for i in range(rows):
+        temp=[]
+        for j in range(cols):
+            if j==0:
+                temp.append(str(input("Enter name*: ")))
+                if temp[j] =='' or temp[j] == ' ':
+                    sys.exit("Name is a madatory field.Process exiting due to blank field...")
+            if j ==1:
+                temp.append(int(input("Enter number*: ")))
+        phone_book.append(temp)
