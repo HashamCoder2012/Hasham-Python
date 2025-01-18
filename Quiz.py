553372516ff1cf8678cb51c6d1fb49f9845c4020
HashamCoder2012 <mhashamk2012@gmail.com>
2025-01-18 11:54:34 +0500

    l

---
+++ Quiz.py
@@ -0,0 +1,7 @@
+import random
+def get_users_choice():
+    user_choice=input("Answer the  Questions?")
+    user_choice=input("Who is the Greatest player of all time?")
+
+def get_computers_choice():
+    computer_choice=input('Ronaldo','Messi')
