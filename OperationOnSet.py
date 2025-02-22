4d4a429b88c55716a8052ca444f75104f9596e3f
HashamCoder2012 <mhashamk2012@gmail.com>
2025-02-22 13:48:04 +0500

    m

---
+++ OperationOnSet.py
@@ -0,0 +1,20 @@
+my_set={1,2,3}
+print(my_set)
+
+my_set={1.0,"Hello",(1,2,3)}
+print(my_set)
+
+
+my_set={1,2,3,4,3,2}
+print(my_set)
+
+my_set={1,2,3,2}
+print(my_set,"\n")
+
+num_set=set([0,1,2,3,4,5])
+print("Original set:")
+print(num_set)
+num_set.remove(5)
+print("After removing the last element from the said set:")
+print(num_set,"\n")
+
