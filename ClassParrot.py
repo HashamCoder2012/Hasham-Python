e1c7b9588b9153508701bf5211353d17318742f3
HashamCoder2012 <mhashamk2012@gmail.com>
2025-03-14 22:14:14 +0500

    n

--- ClassParrot.py
+++ ClassParrot.py
@@ -1,14 +1,14 @@
-class Parrot:
-    species="bird"
+class dog:
+    species="dogs"
     def __init__(self,name,age):
         self.name=name
         self.age=age
-blu=Parrot("Blu",10)
-woo=Parrot("Woo",15)
-print("Blu is a {}".format(blu.species))
-print("Woo is also a {}".format(woo.species))
-print("{} is {} years old".format(blu.name,blu.age))
-print("{} is {}years old".format(woo.name,woo.age))
+sweet=dog("sweet",10012)
+kelly=dog("kelly",19300)
+print("kelly is a {}".format(kelly.species))
+print("sweet is also a {}".format(sweet.species))
+print("{} is {} years old".format(kelly.name,kelly.age))
+print("{} is {}years old".format(sweet.name,sweet.age))
 
 
         
