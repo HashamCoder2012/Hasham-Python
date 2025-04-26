feba205fb48981c40a8c0550ac57fff03575ab77
HashamCoder2012 <mhashamk2012@gmail.com>
2025-04-26 14:16:03 +0500

    hj

--- Getting started it with Widgets.py
+++ Getting started it with Widgets.py
@@ -6,12 +6,19 @@ lbl=Label(text="Enter a number",fg="blue",bg="black")
 lbl2=Label(text="Enter another number",fg="orange",bg="yellow")
 name_entry=Entry()
 name_entry2=Entry()
+resultlabel=Label()
 
 def display():
-    name=name_entry.get()
-    name2=name_entry2.get()
-    btn=Button(text="Calculate",command="Display")
-    text_box=lbl+lbl2
+    n1=name_entry.get()
+    n2=name_entry2.get()
+    prod=n1*n2
+    resultlabel.config(text=prod)
+btn=Button(text="Calculate",command="Display")
+lbl.pack()
+name_entry.pack()
+lbl2.pack()
+name_entry.pack()
+btn.pack()
 root.mainloop()
 
 
