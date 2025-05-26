6c5d81bf8625913f59beb51ba899a0bd9a47f818
HashamCoder2012 <mhashamk2012@gmail.com>
2025-05-26 15:28:00 +0500

    6666

--- Password checker.py
+++ Password checker.py
@@ -3,8 +3,13 @@ from tkinter import messagebox
 root=Tk()
 root.geometry("400x400")
 root.title("Password strenght checker")
-lbl=Label("")
+btn=Button(root,text="your password is weak",color="Green")
+
+label1=Label(root,text="Hey user!,Welcome to Password strenght checker app")
 def msg():
-    MsgBox=messagebox.showinfo("")
+    MsgBox=messagebox.showinfo("Alert","is you password lenght from 6 to 8")
     if MsgBox=="ok":
-        topwin()
+        print("The password is weak")
+    else:
+        print("The password is strong")
+root.mainloop()
---
+++ Python.txt
@@ -0,0 +1,5 @@
+from tkinter import *
+root=Tk()
+root.geometry("400x400")
+root.title("Age Calculator app")
+lbl=Label(text="")
